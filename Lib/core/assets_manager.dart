9702de114a3f8a69cb60f82c95df9808fd6c5b6a
YehiaOthman <yahia22871402@gmail.com>
2024-10-11 08:17:34 +0300

    + sebhaTabLogic + tasbehList + tasbehCounter + sebhaBody.png rotation

--- Lib/core/assets_manager.dart
+++ Lib/core/assets_manager.dart
@@ -6,5 +6,5 @@ class AssetsManager {
   static const String hadithIcon = '${base}hadith_icon.png';
   static const String tasbehIcon = '${base}sebha_icon.png';
   static const String radioIcon = '${base}radio_icon.png';
-  static const String sebhaBody = '${base}sebhaHead.png';
+  static const String sebhaBody = '${base}sebhaBody.png';
 }
